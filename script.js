c268d6b3a3cbb845201513e25abddbf728c70a97
Apoorva.Vanshakruthmath <117889978+ApoorvaVanshakruthmath@users.noreply.github.com>
2023-10-14 11:52:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
 
-    /***const mark={
-        fullname:'mark Mello',
-        mass:718,
-        height:1.99,
-        calcBMI:function(){
-            this.bmi=this.mass/this.height**2;
-            return this.bmi;
-        
-        }
-    }
-
-    const jonas={
-        fullname:'jonas s',
-        mass:92,
-        height:1.95,
-        calcBMI:function(){
-            this.bmi=this.mass/this.height**2;
-            return this.bmi;
-        
-        }
-    }
-    mark.calcBMI();
-    jonas.calcBMI();
-    console.log(mark.bmi);
-    console.log(jonas.bmi);
-    console.log(mark);
-    console.log(`${jonas.fullname} BMI is ${mark.bmi>jonas.bmi?'higher':'lower'} than ${mark.fullname} ${jonas.bmi}` );
-
-    for(let i=0;i<10;i++){
-        console.log(i+1);
-    }***/
-
-/**    var rand=function(val){
-        const x=Math.random()*10;
-        if(val===x){
-            console.log("you won");
-        }
-        else if(val>x){
-            console.log("too big ");
-        }
-        else{
-            console.log("too low");
-        }
-    }
-rand(5);**/
 document.addEventListener('DOMContentLoaded', function () {
     const snum=Math.trunc(Math.random()*20)+1;
     let score=20;
